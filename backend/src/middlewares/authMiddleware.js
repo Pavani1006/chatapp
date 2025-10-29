7367117697dc4873036223cbc37c2c4ad89d0ed6
Pavani1006 <pavanivallem@gmail.com>
2025-10-29 22:43:17 +0530

    JWT verification

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import {User} from "../model/userModel.js";
 
-
+// To protect routes that require authentication
 export const authMiddleware=async(req,res,next)=>{
     try{
         const {token}=req.cookies.token;
